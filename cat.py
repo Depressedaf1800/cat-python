8a9ca975992abab6b9c57b2aaa7a9da831411095
Depressedaf1800 <depressedaf1800@gmail.com>
2020-03-29 22:41:36 +0800

    set up python cat

---
+++ cat.py
@@ -0,0 +1,22 @@
+import discord
+import os
+import time
+
+cat = discord.Client()
+
+@cat.event
+async def on_ready():
+    print('cat is meowing :3')
+
+@cat.event
+async def on_message(message):
+    if message.author == cat.user:
+        return
+
+    if message.content.startswith('!ping'):
+        m = await message.channel.send('pinging...')
+        time.sleep(1)
+        await m.edit(content = f'latency is {round(cat.latency)}')
+
+
+cat.run(os.environ['TOKEN'])
